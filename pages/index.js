35779dcc6b624a35d56179acf1918a7f76c5c521
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-08-17 16:30:15 +0200

    install daisyui

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Hello daisyUI</button>
     </div>
   );
 }
